6f546db1c69382320c1e8370ef97cee39a98da90
Icaro Raphael <icaro2551@gmail.com>
2022-06-01 17:52:14 -0300

    1.0

--- Banco De Dados I/TED 13, 15 e 16/SELECTS.sql
+++ Banco De Dados I/TED 13, 15 e 16/SELECTS.sql
@@ -1,18 +1,6 @@
 use bdi221;
 go
 
---Quantas compras foram feitas em cada forma de pagamento
-select descricao, count(fp.descricao) as quantidade_vendas from forma_pagamento as fp
-inner join venda as v
-on v.id_forma_pagamento = fp.id
-group by fp.descricao
-
---Quantas compras cada cliente realizou
-select nome, count(v.id_cliente) as quantidade from cliente as c
-inner join venda as v
-on v.id_cliente = c.id
-group by c.nome
-order by quantidade
 
 --Qual o valor total que cada cliente comprou
 select c.nome, sum(p.valor) as valor_total from cliente as c
@@ -24,13 +12,6 @@ inner join produto as p
 on p.id = pv.id
 group by c.nome
 order by valor_total desc
-
---Qual pet pertence a qual cliente
-select p.nome as nome_do_pet, c.nome as nome_do_cliente from cliente_pet as cp
-inner join cliente as c
-on c.id = cp.id_cliente
-inner join pet as p
-on p.id = cp.id_pet
 
 --Qual o produto mais caro
 select nome, tipo,valor from produto
